c614df3959e0b3ae2dc4a9098dee4e22c063a00f
rhamaa <firdausnuurr@gmail.com>
2025-10-08 10:09:07 +0700

    new PCI .exe and config for window env

---
+++ RadarLPDP_Autostart.bat
@@ -0,0 +1,3 @@
+    @echo off
+    start "" "C:\Users\gsr-baseband\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\gsr-baseband\Documents\RadarLPDP\main.py"
+    exit
--- config.py
+++ config.py
@@ -32,7 +32,7 @@ POLLING_INTERVAL = 0.5  # Detik (sudah tidak digunakan oleh fft_worker)
 WORKER_REFRESH_INTERVAL = 0.05 # Detik, untuk refresh UI ~20 FPSali per detik
 
 # --- Pengaturan Serial Port ---
-SERIAL_PORT = '/dev/ttyUSB0'  # Port untuk ESP32/Arduino
+SERIAL_PORT = 'COM9'  # Port untuk ESP32/Arduino
 BAUD_RATE = 115200            # Harus sama dengan yang di kode Arduino
 SERIAL_TIMEOUT = 1            # Timeout untuk pembacaan serial (dalam detik)
 
